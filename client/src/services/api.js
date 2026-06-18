5397409d98a84e74857e17901b0d9d075c3a0f29
PRABHAT GAUTAM <prabhatgautam@192.168.1.26>
2026-06-18 16:07:14 +0530

    Frontend deployment config

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8000/api",
+  baseURL: "https://horizontechx-taskflow-realtime.onrender.com/api",
 });
 
 export default api;
